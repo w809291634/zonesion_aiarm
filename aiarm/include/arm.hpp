50ab55a882fa7e72c7e5e6aca12d5ae13c62e710
汪浩 <wanghao@zonesion.comc.cn>
2023-02-23 08:36:11 +0800

    gx

--- aiarm/include/arm.hpp
+++ aiarm/include/arm.hpp
@@ -13,8 +13,10 @@
 #include <moveit/robot_state/robot_state.h>
 #include <moveit/robot_state/conversions.h>
 
-
 namespace Aiarm{
+  using namespace std;
+
+  typedef std::shared_ptr<moveit::planning_interface::MoveGroupInterface> MoveGroupInterfacePtr;
   class arm
   {
     private:
@@ -22,12 +24,25 @@ namespace Aiarm{
       char gripper_ty=1;
       char arm_debug=2;
       std::string PLANNING_GROUP = "manipulator";
-      // moveit::planning_interface::MoveGroupInterface arm(PLANNING_GROUP);
-      moveit::planning_interface::MoveGroupInterface arm_move_group{PLANNING_GROUP};
+      string current_pos="none";
+      moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+
+      // moveit::planning_interface::MoveGroupInterface arm_move_group{PLANNING_GROUP};    // 引用类作为数据方法1
+      Aiarm::MoveGroupInterfacePtr arm_move_group_=nullptr; // 引用类作为数据方法2
 
     public:
       arm(char g_open,char gripper_ty=0,char arm_debug=0);
-      ~arm();
+      ~arm(){};
+
+    void all_gohome(){
+      this->arm_move_group_->setNamedTarget("home");
+      // bool success = (arm_move_group_->plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+      // ROS_INFO_NAMED("tutorial", "Visualizing plan 5 (with no obstacles) %s", success ? "" : "FAILED");
+      // arm_move_group_->execute(my_plan);
+      this->arm_move_group_->move();
+      this->current_pos="all_home";
+    }
+
   };
   // arm::arm(/* args */);
   // arm::~arm();
--- aiarm/src/arm.cpp
+++ aiarm/src/arm.cpp
@@ -14,17 +14,49 @@
 #include <moveit/robot_state/conversions.h>
 
 #include "arm.hpp"
+#include <iostream>  
 
-Aiarm::arm::arm(char g_open,char gripper_ty,char arm_debug):g_open(g_open),gripper_ty(gripper_ty),arm_debug(arm_debug)
-{
-  // static const std::string PLANNING_GROUP = "manipulator";
+namespace  Aiarm{
+  arm::arm(char g_open,char gripper_ty,char arm_debug):g_open(g_open),gripper_ty(gripper_ty),arm_debug(arm_debug)
+  {
+    static const std::string PLANNING_GROUP="manipulator";
+    arm_move_group_.reset(new moveit::planning_interface::MoveGroupInterface(PLANNING_GROUP));
+    arm_move_group_->setMaxVelocityScalingFactor(1.0f);            //设置允许的最大速度和加速度
+    arm_move_group_->setMaxAccelerationScalingFactor(1.0f);
+    /** \brief Set the tolerance that is used for reaching the goal. For
+        joint state goals, this will be distance for each joint, in the
+        configuration space (radians or meters depending on joint type). For pose
+        goals this will be the radius of a sphere where the end-effector must
+        reach. This function simply triggers calls to setGoalPositionTolerance(),
+        setGoalOrientationTolerance() and setGoalJointTolerance(). */
+    // arm_move_group_->setGoalTolerance(0.001)
+    /** \brief Set the joint tolerance (for each joint) that is used for reaching the goal when moving to a joint value
+     * target. */
+    arm_move_group_->setGoalJointTolerance(0.001f);
+    /** \brief Set the position tolerance that is used for reaching the goal when moving to a pose. */
+    arm_move_group_->setGoalPositionTolerance(0.001f);
+    /** \brief Set the orientation tolerance that is used for reaching the goal when moving to a pose. */
+    arm_move_group_->setGoalOrientationTolerance(0.001f);
+
+    arm_move_group_->setPlanningTime(5.0);
+  }
 
 }
-
-
-
 
 int main(int argc, char **argv)
 {
   ros::init (argc, argv, "arm_move_group_tutorial");
+  ros::NodeHandle node_handle;
+
+  // 必须运行ROS旋转，MoveGroupInterface才能获取有关机器人状态的信息。这样做的一种方法是预先启动AsyncSpinner。
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
+  Aiarm::arm test_arm(80);
+  test_arm.all_gohome();
+  while(1){
+    printf("here\r\n");
+    sleep(1);//延时1秒 
+  }
 }
+
